1554fb5eb60c726df0e82224ecbbf4a86d923811
flybear <yu_98@tom.com>
2019-08-22 11:23:43 +0800

    create 3 files

---
+++ print.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print(r"**************************************************************")
+print("I'm ok")
+print('I\'m ok')
+print('Say "hello"')
+print('I\'m learing\n python')
+print('\\\t\\')
+print(r'\\\t\\')
+print('\\\n\\')
+print(r'''hello \n word''')
+print(r'Hello, "Bart"')
+print(r'''Hello,
+lisa!''')
+print('''Hello,
+lisa!''')
+print('Hello,lisa!')
+
+#报错：
+#print('Hello,
+#lisa!')
+
+print('''line1
+line2
+line3''')
